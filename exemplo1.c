b7b91732391a1b2536b46fffb4c649ddbdf66461
Bulka4000 <lucasbulka581@gmail.com>
2024-08-12 22:17:44 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,12 @@
+/* Lab. 1
+* Exemplo 1*/
+#include <stdio.h>
+
+int main(){
+    //imprime mensagem na tela
+    printf("Mackenzie-fci\n");
+    printf("Algoritimos e ");
+    printf("prog.II\n");
+
+    return 0;
+}
